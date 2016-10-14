1c7388085f38206109824531af8f21c7e0068621
JTam3 <johnny@nih.gov>
2016-10-14 10:45:12 -0400

    added deg2rad

--- conversion.py
+++ conversion.py
@@ -1,6 +1,7 @@
 #Code started on 14OCT2016 MRL
 #going to do some implementation of the code. 
 
+import math
 
 def dollars2cents(dollars):
     cents = dollars * 100
@@ -19,3 +20,6 @@ def gallons2liters(gallons):
 def feet2inches(feet)
 	inches = feet * 12
 	return inches
+
+def deg2rad(deg)
+	return deg*math.pi/180
